b9eb2bc9b992c797dd9996e92a7d4fb59a360986
phonarawd <4tencpkr@gmail.com>
2026-08-24 13:22:13 +0900

    feat(admin): register user directory module surface

--- services/api-nest/src/membership/membership.module.ts
+++ services/api-nest/src/membership/membership.module.ts
@@ -3,6 +3,8 @@ import { EventsModule } from "../events/events.module";
 import { MembershipAdminController } from "./membership.admin.controller";
 import { MembershipAdminService } from "./membership.admin.service";
 import { MembershipUserController } from "./membership.user.controller";
+import { UserDirectoryAdminController } from "./user-directory.admin.controller";
+import { UserDirectoryAdminService } from "./user-directory.admin.service";
 
 /**
  * Engine §0.0.7 membership · Admin §9.8.10 force / matchStrictnessOverride.
@@ -10,8 +12,12 @@ import { MembershipUserController } from "./membership.user.controller";
  */
 @Module({
   imports: [EventsModule],
-  controllers: [MembershipAdminController, MembershipUserController],
-  providers: [MembershipAdminService],
-  exports: [MembershipAdminService],
+  controllers: [
+    MembershipAdminController,
+    UserDirectoryAdminController,
+    MembershipUserController,
+  ],
+  providers: [MembershipAdminService, UserDirectoryAdminService],
+  exports: [MembershipAdminService, UserDirectoryAdminService],
 })
 export class MembershipModule {}
