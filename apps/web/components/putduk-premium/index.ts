1e751d263eea874992fcac43d6047d3b996d8910
phonarawd <4tencpkr@gmail.com>
2026-08-26 10:33:09 +0900

    UI: export premium primitives

---
+++ apps/web/components/putduk-premium/index.ts
@@ -0,0 +1,4 @@
+export { PremiumCard, PremiumSurface } from "./PremiumSurface";
+export { PremiumStatus } from "./PremiumStatus";
+export { PremiumMetric } from "./PremiumMetric";
+export { PremiumEmptyState } from "./PremiumEmptyState";
