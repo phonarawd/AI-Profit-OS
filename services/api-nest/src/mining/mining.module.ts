61bb9b3711fbfcc9777014c32b1b8e00c08fd8ff
phonarawd <4tencpkr@gmail.com>
2026-09-21 01:31:09 +0900

    feat(mining): wire phase05 admin module

--- services/api-nest/src/mining/mining.module.ts
+++ services/api-nest/src/mining/mining.module.ts
@@ -1,5 +1,8 @@
 import { Module } from "@nestjs/common";
+import { KillSwitchModule } from "../kill-switch/kill-switch.module";
 import { LedgerModule } from "../ledger/ledger.module";
+import { MiningAdminController } from "./mining.admin.controller";
+import { MiningAdminService } from "./mining-admin.service";
 import { MineCatalogController, MiningController } from "./mining.controller";
 import { MiningInternalController } from "./mining.internal.controller";
 import { MiningOperationCoordinatorService } from "./mining-operation-coordinator.service";
@@ -8,13 +11,19 @@ import { MiningReadService } from "./mining-read.service";
 import { MiningService } from "./mining.service";
 
 @Module({
-  imports: [LedgerModule],
-  controllers: [MineCatalogController, MiningController, MiningInternalController],
+  imports: [LedgerModule, KillSwitchModule],
+  controllers: [
+    MineCatalogController,
+    MiningController,
+    MiningInternalController,
+    MiningAdminController,
+  ],
   providers: [
     MiningService,
     MiningProfitEngineService,
     MiningReadService,
     MiningOperationCoordinatorService,
+    MiningAdminService,
   ],
   exports: [MiningService],
 })
