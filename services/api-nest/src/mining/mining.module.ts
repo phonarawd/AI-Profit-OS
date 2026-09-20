8d605a4bcf21c702480b1998ec49eda75732addf
phonarawd <4tencpkr@gmail.com>
2026-09-21 01:03:25 +0900

    feat(mining): wire phase04 mining services

--- services/api-nest/src/mining/mining.module.ts
+++ services/api-nest/src/mining/mining.module.ts
@@ -1,8 +1,21 @@
 import { Module } from "@nestjs/common";
 import { LedgerModule } from "../ledger/ledger.module";
-import { MiningController } from "./mining.controller";
+import { MineCatalogController, MiningController } from "./mining.controller";
+import { MiningInternalController } from "./mining.internal.controller";
+import { MiningOperationCoordinatorService } from "./mining-operation-coordinator.service";
 import { MiningProfitEngineService } from "./mining-profit-engine.service";
+import { MiningReadService } from "./mining-read.service";
 import { MiningService } from "./mining.service";
 
-@Module({ imports: [LedgerModule], controllers: [MiningController], providers: [MiningService, MiningProfitEngineService], exports: [MiningService] })
+@Module({
+  imports: [LedgerModule],
+  controllers: [MineCatalogController, MiningController, MiningInternalController],
+  providers: [
+    MiningService,
+    MiningProfitEngineService,
+    MiningReadService,
+    MiningOperationCoordinatorService,
+  ],
+  exports: [MiningService],
+})
 export class MiningModule {}
